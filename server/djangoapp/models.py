265d870201a54b2d435979487c7fb5d53803cdf0
knocek <knocek123@o2.pl>
2026-09-08 05:47:06 -0400

    fix python lint

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
@@ -11,6 +10,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -28,6 +28,7 @@ class CarMake(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
 
 class CarModel(models.Model):
     CAR_TYPES = [
